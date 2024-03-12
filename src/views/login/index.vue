5bc92b2595b2e8f34d22d53dee9469011a4f0f04
guowei <wei.guo@wenge.com>
2024-03-12 17:42:18 +0800

    feat: 登录页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,76 @@
 <template>
   <div class="login-container">
+    <div class="login-main">
+      <div class="left-position">
+        <img src="../../assets/login/position.png"/>
+      </div>
+      <div class="right-position">
+        <img src="../../assets/login/logo.png"/>
+        <div class="login-title">大宗贸易综合服务平台</div>
+        <div class="login-msg">欢迎登录</div>
+        <div class="login-form">
+          <!-- 登录表单 -->
+          <el-form
+            ref="loginFormRef"
+            :model="loginData"
+            :rules="loginRules"
+            class="login-form"
+          >
+            <el-form-item prop="user_name" :style="{width: '360px', height: '48px'}">
+              <div class="p-2">
+                <svg-icon icon-class="user" />
+              </div>
+              <el-input
+                ref="username"
+                v-model="loginData.user_name"
+                class="flex-1"
+                :placeholder="$t('login.username')"
+                name="username"
+              />
+            </el-form-item>
+
+            <el-tooltip
+              :disabled="isCapslock === false"
+              content="Caps lock is On"
+              placement="right"
+            >
+              <el-form-item prop="password">
+                <span class="p-2">
+                  <svg-icon icon-class="password" />
+                </span>
+                <el-input
+                  v-model="loginData.password"
+                  class="flex-1"
+                  :placeholder="$t('login.password')"
+                  :type="passwordVisible === false ? 'password' : 'input'"
+                  size="large"
+                  name="password"
+                  @keyup="checkCapslock"
+                  @keyup.enter="handleLogin"
+                />
+                <span class="mr-2" @click="passwordVisible = !passwordVisible">
+                  <svg-icon
+                    :icon-class="passwordVisible === false ? 'eye' : 'eye-open'"
+                    class="cursor-pointer"
+                  />
+                </span>
+              </el-form-item>
+            </el-tooltip>
+
+            <w-button
+              :loading="loading"
+              type="primary"
+              class="w-full"
+              :style="{width: '360px', height: '48px'}"
+              @click.prevent="handleLogin"
+              >{{ $t("login.login") }}
+            </w-button>
+          </el-form>
+        </div>
+      </div>
+    </div>
     <!-- 登录表单 -->
-    <el-card
+    <!-- <el-card
       class="z-1 !border-none w-100 !bg-transparent !rounded-4% <sm:w-83"
     >
       <el-form
@@ -60,16 +129,16 @@
           >{{ $t("login.login") }}
         </el-button>
       </el-form>
-    </el-card>
+    </el-card> -->
 
     <!-- ICP备案 -->
-    <div
+    <!-- <div
       class="absolute bottom-1 text-[10px] text-center"
       v-show="useAppStore().device == 'desktop'"
     >
       <p>Copyright © 2021 - 2023 anonymity 版权所有</p>
       <p>皖ICP备xxx号</p>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -204,6 +273,56 @@ onMounted(() => {
 </script>
 
 <style lang="scss" scoped>
+@font-face {
+  font-family: myFont;
+  src: url('@/assets/iconfont/aliht.ttf')
+}
+.login-main {
+  display: flex;
+  justify-content: center;
+  .left-position {
+    padding: 60px 24px 60px 8px;
+    background-color: rgba(205, 226, 255, 1);
+    border-radius: 12px 0px 0px 12px;
+    img {
+      width: 554px;
+      height: 520px;
+    }
+  }
+  .right-position {
+    border-radius: 0px 12px 12px 0px;
+    background-color: #fff;
+    width: 457px;
+  }
+}
+
+.right-position{
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding-top: 78px;
+  img {
+    width: 64px;
+    height: 52px;
+  }
+  .login-title {
+    font-family: myFont;
+    font-weight: bold;
+    font-size: 30px;
+    color: #333333;
+    line-height: 36px;
+    margin-top: 14px;
+  }
+  .login-msg {
+    font-size: 20px;
+    color: #333;
+    line-height: 33px;
+    font-weight: 300;
+    margin-top: 20px;
+  }
+}
+
 .dark .login-container {
   background: url("@/assets/images/login-bg-dark.jpg") no-repeat center right;
 }
@@ -212,7 +331,8 @@ onMounted(() => {
   @apply w-full h-full flex-center;
 
   overflow-y: auto;
-  background: url("@/assets/images/login-bg.jpg") no-repeat center right;
+  background-image: url("@/assets/login/bg.png");
+  // background: url("@/assets/images/login-bg.jpg") no-repeat center right;
 
   .login-form {
     padding: 30px 10px;
