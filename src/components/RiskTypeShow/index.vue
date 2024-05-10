1231217ca94bb3be01048228d1e3b9269080f4a0
guowei <wei.guo@wenge.com>
2024-05-10 15:01:43 +0800

    feat: 风险下钻通一

--- src/components/RiskTypeShow/index.vue
+++ src/components/RiskTypeShow/index.vue
@@ -1,23 +1,41 @@
 <template>
   <div>
-    <div class="risk-result" @click="handleResult">
+    <div v-if="info?.result && info?.result.length < 15" class="risk-result" @click="handleResult">
       {{ info?.result }}
     </div>
-
-    <w-modal v-model:visible="dialogTableVisible" @ok="handleBeforeClose" ok-text="知道了" :hide-cancel="true" :width="1200">
+    <w-tooltip v-else>
+      <div class="risk-result" @click="handleResult">
+        {{ info?.result.substring(0, 14) }}...
+      </div>
+      <template #content>{{ info?.result }}</template>
+    </w-tooltip>
+    <w-modal v-model:visible="dialogTableVisible" ok-text="知道了" :hide-cancel="true" :width="1200">
       <template #title>
-        <div class="flex-base-start">
+        <div class="modal-header flex-base-start">
           <img src="@/assets/base/waring.svg">
           <div class="risk-title">{{ info?.name }}异常</div>
         </div>
       </template>
-      <abnormal-ty v-if="riskType == abnormalStatus.TY" :info="info" :columns="columns"></abnormal-ty>
-      <abnormal-djpl v-else-if="riskType == abnormalStatus.DJPL" :info="info" :columns="columns"></abnormal-djpl>
-      <abnormal-cfwjy v-else-if="riskType == abnormalStatus.CFWJY" :info="info" :columns="columns"></abnormal-cfwjy>
-      <abnormal-jxxsp v-else-if="riskType == abnormalStatus.JXXSP" :info="info"></abnormal-jxxsp>
-      <abnormal-glyc v-else-if="riskType == abnormalStatus.GLYC" :info="info" :columns="columns"></abnormal-glyc>
-      <abnormal-htzt v-else-if="riskType == abnormalStatus.HTZT" :info="info" :columns="columns"></abnormal-htzt>
-      <abnormal-jygm v-else-if="riskType == abnormalStatus.JYGM" :info="info" :columns="columns"></abnormal-jygm>
+      <el-tabs type="card" v-if="sourceList.length > 1">
+        <el-tab-pane :label="item.key" v-for="(item, i) in sourceList" :key="i">
+          <abnormal-ty v-if="riskType == abnormalStatus.TY" :info="item" :columns="columns"></abnormal-ty>
+          <abnormal-djpl v-else-if="riskType == abnormalStatus.DJPL" :info="info" :columns="columns"></abnormal-djpl>
+          <abnormal-cfwjy v-else-if="riskType == abnormalStatus.CFWJY" :info="info" :columns="columns"></abnormal-cfwjy>
+          <abnormal-jxxsp v-else-if="riskType == abnormalStatus.JXXSP" :info="info"></abnormal-jxxsp>
+          <abnormal-glyc v-else-if="riskType == abnormalStatus.GLYC" :info="info" :columns="columns"></abnormal-glyc>
+          <abnormal-htzt v-else-if="riskType == abnormalStatus.HTZT" :info="info" :columns="columns"></abnormal-htzt>
+          <abnormal-jygm v-else-if="riskType == abnormalStatus.JYGM" :info="info" :columns="columns"></abnormal-jygm>
+        </el-tab-pane>
+      </el-tabs>
+      <div v-else>
+        <abnormal-ty v-if="riskType == abnormalStatus.TY" :info="info" :columns="columns"></abnormal-ty>
+        <abnormal-djpl v-else-if="riskType == abnormalStatus.DJPL" :info="info" :columns="columns"></abnormal-djpl>
+        <abnormal-cfwjy v-else-if="riskType == abnormalStatus.CFWJY" :info="info" :columns="columns"></abnormal-cfwjy>
+        <abnormal-jxxsp v-else-if="riskType == abnormalStatus.JXXSP" :info="info"></abnormal-jxxsp>
+        <abnormal-glyc v-else-if="riskType == abnormalStatus.GLYC" :info="info" :columns="columns"></abnormal-glyc>
+        <abnormal-htzt v-else-if="riskType == abnormalStatus.HTZT" :info="info" :columns="columns"></abnormal-htzt>
+        <abnormal-jygm v-else-if="riskType == abnormalStatus.JYGM" :info="info" :columns="columns"></abnormal-jygm>
+      </div>
     </w-modal>
   </div>
 </template>
@@ -28,13 +46,20 @@ import { abnormalStatus } from './type'
 
 const props = defineProps({
   info: { // 风险信息
-    default: {}
+    default: {
+      result: '',
+      source: '',
+      fieldMapping:'',
+      handleMethod: -1,
+      name: ''
+    }
   }
 })
 
 const dialogTableVisible = ref(false)
 const riskType = ref(-1); // 根据风险类型控制页面显示
 const columns = ref([])
+const sourceList = ref([])
 
 const getWidth = computed(() => {
   if(riskType.value == 1){
@@ -45,7 +70,7 @@ const getWidth = computed(() => {
 })
 
 // 处理数据，生成columns
-function formateWord(d) {
+function formateWord(d: any) {
   columns.value = []
   const arr = [{
     title: "序号",
@@ -61,9 +86,9 @@ function formateWord(d) {
       ellipsis: true,
       tooltip: {position: 'left'},
     }
-    arr.push(cur)
+    arr.push(cur as any)
   })
-  columns.value = arr;
+  columns.value = arr as any;
 }
 
 
@@ -71,13 +96,24 @@ function formateWord(d) {
 function handleResult() {
   dialogTableVisible.value = true;
   riskType.value = props.info.handleMethod
-  // console.log('mapding--------------:', props.info.fieldMapping)
-  formateWord(JSON.parse(props.info.fieldMapping)); // 字段对应关系
+  if(props.info.source) {
+    sourceList.value = JSON.parse(props.info.source)
+    sourceList.value[0].source[0].includedDate = '222222'
+  }
+  console.log('source--------------:', JSON.parse(props.info.source))
+  props.info.fieldMapping && formateWord(JSON.parse(props.info.fieldMapping)); // 字段对应关系
+
 }
 
 </script>
 
 <style scoped lang="scss">
+:deep(.w-modal-body) {
+  padding: 16px 30px;
+}
+.modal-header {
+  width: 100%;
+}
 .risk-result {
   color:#3470FF;
   cursor: pointer;
