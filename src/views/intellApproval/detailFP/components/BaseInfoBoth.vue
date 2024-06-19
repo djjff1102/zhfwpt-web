a1448a1a69e12044907bdb87ccbb89487078eef4
guowei <wei.guo@wenge.com>
2024-06-19 17:32:22 +0800

    feat: 发票和受票

--- src/views/intellApproval/detailFP/components/BaseInfoBoth.vue
+++ src/views/intellApproval/detailFP/components/BaseInfoBoth.vue
@@ -2,13 +2,13 @@
   <!-- 销项 -->
   <el-descriptions class="margin-top" :column="2" border>
     <el-descriptions-item label="开票单位" width="60">
-      {{ order?.receivingCompanyName || '--' }}
-    </el-descriptions-item>
-    <el-descriptions-item label="开票单位统一社会信用码" width="60">{{ order?.receivingCreditNo || '--' }}</el-descriptions-item>
-    <el-descriptions-item label="受票单位" width="60">
       {{ order?.invoicingCompanyName || '--' }}
     </el-descriptions-item>
-    <el-descriptions-item label="受票单位统一社会信用码" width="60">{{ order?.invoicingCreditNo || '--' }}</el-descriptions-item>
+    <el-descriptions-item label="开票单位统一社会信用码" width="60">{{ order?.invoicingCreditNo || '--' }}</el-descriptions-item>
+    <el-descriptions-item label="受票单位" width="60">
+      {{ order?.receivingCompanyName || '--' }}
+    </el-descriptions-item>
+    <el-descriptions-item label="受票单位统一社会信用码" width="60">{{ order?.receivingCreditNo || '--' }}</el-descriptions-item>
     <el-descriptions-item label="开票日期">
       {{ order?.invoicingDate || '--' }}
     </el-descriptions-item>
