12b87ee970a2855a432686b6052bbbc95d46d79c
xiaoyue <yue.xiao@wenge.com>
2024-07-02 18:00:51 +0800

    fix(TendencyChart): :bug: 修改进销项发票金额单位

--- src/components/InvoiceInformation/components/TendencyChart/index.vue
+++ src/components/InvoiceInformation/components/TendencyChart/index.vue
@@ -9,56 +9,56 @@ import { onMounted, watch, onUnmounted } from "vue";
 
 const props = defineProps({
   time: {
-    default: []
+    default: [],
   },
   dataList: {
-    default: []
+    default: [],
   },
   chartId: {
-    default: 'tendencyChart'
-  }
-})
+    default: "tendencyChart",
+  },
+});
 
 const echartData = ref({
-    // color: ['#5ECF69', '#3470FF'],
-    title: {
-      text: "单位：元",
-      left: "left",
-      textStyle: {
-        fontSize: 14,
-        color: "#999",
-        fontWeight: 500,
-        fontStyle: "normal",
-      },
+  // color: ['#5ECF69', '#3470FF'],
+  title: {
+    text: "单位：万元",
+    left: "left",
+    textStyle: {
+      fontSize: 14,
+      color: "#999",
+      fontWeight: 500,
+      fontStyle: "normal",
     },
-    tooltip: {
-      trigger: "axis",
-      axisPointer: {
-        animation: true,
-      },
+  },
+  tooltip: {
+    trigger: "axis",
+    axisPointer: {
+      animation: true,
     },
-    grid: [
-      {
-        left: 120,
-        right: 60,
-      },
-    ],
-    legend: {},
-    xAxis: [
-      {
-        type: "category",
-        boundaryGap: false,
-        data: [],
-      },
-    ],
-    yAxis: [
-      {
-        name: "",
-        type: "value",
-      },
-    ],
-    series: [],
-  })
+  },
+  grid: [
+    {
+      left: 120,
+      right: 60,
+    },
+  ],
+  legend: {},
+  xAxis: [
+    {
+      type: "category",
+      boundaryGap: false,
+      data: [],
+    },
+  ],
+  yAxis: [
+    {
+      name: "",
+      type: "value",
+    },
+  ],
+  series: [],
+});
 
 watch(
   () => props.dataList,
@@ -66,26 +66,29 @@ watch(
     nextTick(() => {
       echartData.value.series = v.series;
       echartData.value.xAxis[0].data = props.time;
-      init()
-    })
-  }, {
+      init();
+    });
+  },
+  {
     deep: true,
   }
 );
-const TChert = ref()
+const TChert = ref();
 function init() {
   const tendencyDom = document.getElementById(props.chartId);
   const tendencyChart = echarts.init(tendencyDom, null, {
     width: "auto",
     height: "400",
   });
-  TChert.value = tendencyChart
+  TChert.value = tendencyChart;
   tendencyChart.setOption(echartData.value);
-  window.addEventListener('resize', handleResize);
+  window.addEventListener("resize", handleResize);
 }
 
 function handleResize() {
-  const chart = echarts.getInstanceByDom(document.getElementById(props.chartId));
+  const chart = echarts.getInstanceByDom(
+    document.getElementById(props.chartId)
+  );
   if (chart) {
     chart.resize();
   }
@@ -96,9 +99,7 @@ function handleResize() {
 // })
 
 onUnmounted(() => {
-  window.removeEventListener('resize', handleResize);
-})
-
+  window.removeEventListener("resize", handleResize);
+});
 </script>
-<style lang="scss">
-</style>
+<style lang="scss"></style>
